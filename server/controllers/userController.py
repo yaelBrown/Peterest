4c03d04b78f03c696aaa3ce1457e432a15feb677
Yael Brown <yaelrbrown@gmail.com>
2020-02-01 22:49:00 -0600

    Able to hash passwords on backend.

--- server/controllers/userController.py
+++ server/controllers/userController.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, request
 from flask import Blueprint
-from flask_restful import Resource, Api
+
 # from flask_mysqldb import flask_mysqldb
-# from flask.ext.bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 userController = Blueprint('userController', __name__)
+
+_rounds = 12
+_nothing = None
 
 @userController.route('/login')
 def login():
   return 'You are logged in'
+
+@userController.route('/register', methods=['GET', 'POST'])
+def register():
+  if request.method == 'GET':
+    return "This request must be a POST"
+  if request.method == 'POST':
+    return "good this is a post"
+
+@userController.route('/test')
+def test():
+  password = 'hunter2'
+  print("pw is {}".format(password))
+
+  pw_hash = Bcrypt.generate_password_hash(_nothing, password, _rounds)
+
+  return pw_hash
