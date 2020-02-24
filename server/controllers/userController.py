60dd77add551bbf9f858cd06931e3c07498644c9
Yael Brown <yaelrbrown@gmail.com>
2020-02-23 20:30:56 -0600

    finished register, used it to add users.

--- server/controllers/userController.py
+++ server/controllers/userController.py
@@ -10,7 +10,7 @@ userController = Blueprint('userController', __name__)
 _rounds = 12
 _nothing = None
 
-# con = pymysql.Connect('::1', 'root', 'codeup', 'Peterest', 'utf8mb4', cursorclass=pymysql.cursors.DictCursor)
+con = pymysql.Connect(host='localhost', user='root', password='codeup', db='Peterest', charset='utf8', cursorclass=pymysql.cursors.DictCursor, port=3306)
 
 @userController.route('/login')
 def login():
@@ -21,14 +21,14 @@ def register():
   if request.method == 'GET':
     return jsonify({"msg": "This must be a post!"}), 200
   if request.method == 'POST':
-    con = pymysql.Connect(host='localhost', user='root', password='codeup', db='Peterest', charset='utf8', cursorclass=pymysql.cursors.DictCursor, port=3306)
     data = request.get_json()
 
     newUser = {}
     newUser["username"] = data["username"]
     newUser["pw"] = Bcrypt.generate_password_hash(_nothing, data["password"], _rounds)
-    newUser["isAdmin"] = False
+    newUser["isAdmin"] = data["isAdmin"]
     newUser["name"] = data["name"]
+
     print(newUser)
     try:
       with con.cursor() as cur:
@@ -36,9 +36,9 @@ def register():
         cur.execute(sql, (newUser["username"], newUser["pw"], int(newUser["isAdmin"]), newUser["name"]))
         con.commit()
     finally:
-      con.close()
+      print("\n")
 
-  return "??"
+    return {"msg": "New user {} added".format(newUser["username"])}
 
 @userController.route('/test')
 def test():
