3b83eda53e92364a2d33e19d5da6d79a6162e7aa
Yael Brown <yaelrbrown@gmail.com>
2020-10-17 12:23:02 -0500

    restyled login page, created api.js for axios connections

--- client/src/views/Login.js
+++ client/src/views/Login.js
@@ -7,44 +7,55 @@ class Login extends React.Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleForget = this.handleSubmit.bind(this);
   }
 
-  handleSubmit = (event) => {
-    alert("Alert button was pressed");
+  handleSubmit = event => {
+    alert("Login button was pressed");
     window.location.href = "/dashboard";
+    event.preventDefault();
+  }
+
+  handleRegister = event => {
+    alert("Register button was pressed");
+    window.location.href = "/register";
+    event.preventDefault();
+  }
+
+  handleForget = event => {
+    alert("Forget was pressed");
+    window.location.href = "/forget";
     event.preventDefault();
   }
   
   render() {
     return (
-      <div>
-        <div id="loginContainer">
-          <div id="loginLeft">
-            <img id="leftBgImage" src={require('../assets/img/lilgriffybob.jpg')} alt=""/>
-          </div>
-          <div id="loginRight">
-            <div id="loginForm">
-              <PeterestLogo/>
-              <h3>A place for pets</h3>
-                <form>
-                  <p>Login :)</p>
-                  <div className="loginFormRows">
-                    <h3>Username</h3>
-                    <input id="username" type="text"></input>
-                  </div>
-                  <div className="loginFormRows">
-                    <h3>Password</h3>
-                    <input id="password" type="password"></input>  
-                  </div>
-                  <div className="loginFormRows">
-                    <button type="submit" value="Submit" onClick={this.handleSubmit}>Submit</button>
-                    <button type="">Register</button>
-                  </div>
-                  <div className="loginFormRows">
-                    <a href="/forgot">Forgot your account?</a>
-                  </div>
-                </form>
-            </div>
+      <div id="loginContainer">
+        <div id="loginRight">
+          <div id="loginForm">
+            <PeterestLogo/>
+            <h3>A place for pets</h3>
+            <p>Login :)</p>
+              <form>
+                <table id="loginTable">
+                  <tr>
+                    <td><h3>Username</h3></td>
+                    <td><input id="username" type="text" class="loginTableInputField"></input></td>
+                  </tr>
+                  <tr>
+                    <td><h3>Password</h3></td>  
+                    <td><input id="password" type="password" class="loginTableInputField"></input></td>
+                  </tr>
+                </table>
+                <div className="loginFormRows">
+                  <button type="submit" value="Submit" onClick={this.handleSubmit}>Submit</button>
+                  <button onClick={this.handleRegister}>Register</button>
+                </div>
+                <div className="loginFormRows">
+                  <a href="/forget">Forgot your account?</a>
+                </div>
+              </form>
           </div>
         </div>
       </div>
