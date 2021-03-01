4460025a3958a392cd4aaa949f0dab6b785c45da
Yael Brown <yaelrbrown@gmail.com>
2021-02-28 23:16:18 -0600

    ran git update

--- client/src/Router.js
+++ client/src/Router.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import { Switch, Route } from "react-router-dom";
 
 import Home from "./views/Home.js";
-import Dashboard from "./views/Dashboard.js";
-import Friends from "./views/Friends.js";
-import Settings from "./views/Settings.js";
-import Profile from "./views/Profile.js";
-import Albums from "./views/Albums.js";
+// import Dashboard from "./views/Dashboard.js";
+// import Friends from "./views/Friends.js";
+// import Settings from "./views/Settings.js";
+// import Profile from "./views/Profile.js";
+// import Albums from "./views/Albums.js";
 
 export default class Router extends Component {
   render() {
     return (
       <Switch>
         <Route path="/" exact component={Home}/>
-        <Route path="/dashboard" exact component={Dashboard}/>
+        {/* <Route path="/dashboard" exact component={Dashboard}/>
         <Route path="/friends" exact component={Friends}/>
         <Route path="/settings" exact component={Settings}/>
         <Route path="/profile" exact component={Profile}/>
-        <Route path="/albums" exact component={Albums}/>
+        <Route path="/albums" exact component={Albums}/> */}
       </Switch>
     )
   }
